e31c3eaadca53154ef0092c3730c77e2c157a7c7
Mark <mark.moberts@sogeti.com>
2025-01-12 00:06:35 +0100

    chore: update version to 0.1.8 and change Python dependency to 3.12

--- setup.py
+++ setup.py
@@ -38,131 +38,95 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
     ],
-    long_description="""Robot Framework MITM Library
-============================
+    long_description="""
+# Robot Framework MITM Library
 
-.. figure:: https://robotframework.org/img/RF.svg
-   :alt: Robot Framework Logo
+![Mitm Library Logo](https://raw.githubusercontent.com/MobyNL/robotframework-mitmlibrary/refs/heads/main/MITMLibrary_small.png)
 
-   Robot Framework Logo
+## Keyword Documentation
 
-Keyword
--------
+[Keyword documentation](https://mobynl.github.io/robotframework-mitmlibrary/MitmLibraryKeywords.html)
 
-`Keyword
-documentation <https://mobynl.github.io/robotframework-mitmlibrary/MitmLibraryKeywords.html>`__
+## Overview
 
-Overview
---------
+The Robot Framework MITM Library is a custom library for [Robot Framework](https://robotframework.org/) that enables integration with the Python package [mitm](https://github.com/mitmproxy/mitmproxy). This library allows you to automate and test scenarios involving Man-in-the-Middle (MITM) proxy functionality within your Robot Framework test suites.
 
-The Robot Framework MITM Library is a custom library for `Robot
-Framework <https://robotframework.org/>`__ that enables integration with
-the Python package `mitm <https://github.com/mitmproxy/mitmproxy>`__.
-This library allows you to automate and test scenarios involving
-Man-in-the-Middle (MITM) proxy functionality within your Robot Framework
-test suites.
+If you need help, have suggestions or want to discuss anything, feel free to contact through the [Slack channel](https://robotframework.slack.com/archives/C06M2J3J8AC).
 
-If you need help, have suggestions or want to discuss anything, feel
-free to contact through the `slack
-channel <https://robotframework.slack.com/archives/C06M2J3J8AC>`__.
+## Features
 
-Features
---------
+- Interact with MITM proxy using Robot Framework keywords.
+- Manipulate network traffic for testing purposes.
+- Easily simulate different network conditions and responses.
+- Integrate MITM proxy capabilities into your existing Robot Framework tests.
 
--  Interact with MITM proxy using Robot Framework keywords.
--  Manipulate network traffic for testing purposes.
--  Easily simulate different network conditions and responses.
--  Integrate MITM proxy capabilities into your existing Robot Framework
-   tests.
-
-Installation
-------------
+## Installation
 
 1. Install Robot Framework (if not already installed):
 2. Install mitm library using pip:
 
-::
+```sh
+pip install robotframework-mitmlibrary
+```
 
-   pip install robotframework-mitmlibrary
-
-Usage
------
+## Usage
 
 1. Import the MITM Library in your Robot Framework test suite:
 
-.. code:: robotframework
+```robotframework
+*** Settings ***
+Library       MitmLibrary
+```
 
-   *** Settings ***
-   Library       MitmLibrary
+2. Use the available keywords to interact with the MITM proxy and manipulate network traffic as needed:
 
-2. Use the available keywords to interact with the MITM proxy and
-   manipulate network traffic as needed:
+```robotframework
+*** Settings ***
+Library       MitmLibrary
 
-.. code:: robotframework
+*** Test Cases ***
+Block and Delay Websites
+    Start Mitm Proxy
 
-   *** Settings ***
-   Library       MitmLibrary
+    # Block requests to Robot Framework website
+    Add To Blocklist    robotframework.org
 
-   *** Test Cases ***
-   Block and Delay Websites
-       Start Mitm Proxy
+    # Delay requests to Google
+    Add Response Delay  GoogleDelay   https://www.google.com  5  # Delay for 5 seconds
 
-       # Block requests to Robot Framework website
-       Add To Blocklist    robotframework.org
+    # Perform tests that involve network traffic manipulation
+    # ...
 
-       # Delay requests to Google
-       Add Response Delay  GoogleDelay   https://www.google.com  5  # Delay for 5 seconds
+    Stop Mitm Proxy
+```
 
-       # Perform tests that involve network traffic manipulation
-       # ...
+## Why use Mitm?
 
-       Stop Mitm Proxy
+Mitm allows manipulation on single browser instance, by using a proxy. It does not require you to set up stubs or mocks that might influence the entire application at once, also resulting in stubbed/mocked behaviour while manual testing.
 
-Why use Mitm?
-~~~~~~~~~~~~~
+Examples where Mitm is useful:
+- When running in parallel, if you do not want your other instances to be influenced.
+- Manipulate the response of a request to see how the front end handles it.
+- When stubs or mocks are not available or their behaviour is not sufficient for your testing needs.
+- When you want to have full control as tester, without dependency on a developer.
 
-Mitm allows manipulation on single browser instance, by using a proxy.
-It does not require you to set up stubs or mocks that might influence
-the entire application at once, also resulting in stubbed/mocked
-behaviour while manual testing.
+## Mitm Certificates
 
-Examples where Mitm is useful: - When running in parallel, if you do not
-want your other instances to be influenced. - Manipulate the response of
-a request to see how the front end handles it - When stubs or mocks are
-not available or their behaviour is not sufficient for your testing
-needs. - When you want to have full control as tester, without
-dependency on a developer
+To test with SSL verification, or use a browser without ignoring certificates, you will need to set up the certificates related to mitm. Follow the guide on the [Mitm website](https://docs.mitmproxy.org/stable/concepts-certificates/).
 
-Mitm Certificates
-~~~~~~~~~~~~~~~~~
+## Documentation
 
-To test with SSL verification, or use a browser without ignoring
-certificates, you will need to set up the certificates related to mitm.
-Follow the guide on the `Mitm
-website <https://docs.mitmproxy.org/stable/concepts-certificates/>`__
+For detailed information on the available keywords and usage examples, please refer to the [Keyword Documentation](https://mobynl.github.io/robotframework-mitmlibrary/MitmLibraryKeywords.html).
 
-Documentation
--------------
+## Contributing
 
-For detailed information on the available keywords and usage examples,
-please refer to the `Keyword
-Documentation <https://mobynl.github.io/robotframework-mitmlibrary/MitmLibraryKeywords.html>`__
+Contributions are welcome! If you encounter any issues, have suggestions for improvements, or would like to add new features, feel free to open an issue or submit a pull request.
 
-Contributing
-------------
-
-Contributions are welcome! If you encounter any issues, have suggestions
-for improvements, or would like to add new features, feel free to open
-an issue or submit a pull request.
-
-License
--------
+## License
 
 This project is licensed under the MIT License.
 
-Note: This project is not officially affiliated with or endorsed by the
-mitmproxy project or robotframework.
+Note: This project is not officially affiliated with or endorsed by the mitmproxy project or robotframework.
 """,
-    readme="readme.md",
-    long_description_content_type="text/x-rst",
+    long_description_content_type="text/markdown",
 )
