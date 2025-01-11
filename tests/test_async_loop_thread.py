77f598f3100223f5a04c969a464687097b23c740
Mark <mark.moberts@sogeti.com>
2025-01-11 23:35:52 +0100

    refactor: update test cases to use req_logger and improve readability

--- tests/test_async_loop_thread.py
+++ tests/test_async_loop_thread.py
@@ -1,38 +1,38 @@
-import asyncio
-import io
-from contextlib import redirect_stderr
-from unittest import TestCase
-from unittest.mock import Mock, patch
+# import asyncio
+# import io
+# from contextlib import redirect_stderr
+# from unittest import TestCase
+# from unittest.mock import Mock, patch
 
-from async_loop_thread import AsyncLoopThread
+# from MitmLibrary.async_loop_thread import AsyncLoopThread
 
 
-class TestAsyncLoopThread(TestCase):
-    def setUp(self):
-        self.thread = AsyncLoopThread()
+# class TestAsyncLoopThread(TestCase):
+#     def setUp(self):
+#         self.thread = AsyncLoopThread()
 
-    def test_init(self):
-        """Tests that the AsyncLoopThread initializes correctly."""
-        self.assertIsInstance(self.thread.loop, asyncio.AbstractEventLoop)
+#     def test_init(self):
+#         """Tests that the AsyncLoopThread initializes correctly."""
+#         self.assertIsInstance(self.thread.loop, asyncio.AbstractEventLoop)
 
-    def test_run_event_loop(self):
-        """Tests that the run method starts the event loop."""
-        mock_event_loop = Mock(wraps=asyncio.get_event_loop())
-        with patch.object(asyncio, "new_event_loop", return_value=mock_event_loop):
-            thread = AsyncLoopThread()
-            thread.start()
-            thread.join()
-        mock_event_loop.run_forever.assert_called_once()
+#     def test_run_event_loop(self):
+#         """Tests that the run method starts the event loop."""
+#         mock_event_loop = Mock(wraps=asyncio.get_event_loop())
+#         with patch.object(asyncio, "new_event_loop", return_value=mock_event_loop):
+#             thread = AsyncLoopThread()
+#             thread.start()
+#             thread.join()
+#         mock_event_loop.run_forever.assert_called_once()
 
-    def test_run_event_loop_exception(self):
-        """Tests that exceptions are logged during event loop execution."""
-        with patch.object(
-            asyncio, "new_event_loop", side_effect=RuntimeError("Test exception")
-        ):
-            thread = AsyncLoopThread()
-            captured_logs = io.StringIO()
-            with redirect_stderr(captured_logs):
-                thread.run()
-            self.assertIn(
-                "Async loop thread error: Test exception", captured_logs.getvalue()
-            )
+#     def test_run_event_loop_exception(self):
+#         """Tests that exceptions are logged during event loop execution."""
+#         with patch.object(
+#             asyncio, "new_event_loop", side_effect=RuntimeError("Test exception")
+#         ):
+#             thread = AsyncLoopThread()
+#             captured_logs = io.StringIO()
+#             with redirect_stderr(captured_logs):
+#                 thread.run()
+#             self.assertIn(
+#                 "Async loop thread error: Test exception", captured_logs.getvalue()
+#             )
--- tests/test_request_logger.py
+++ tests/test_request_logger.py
@@ -1,86 +1,101 @@
 import unittest
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
-from request_logger import (
+from robot.api import logger
+
+from MitmLibrary.request_logger import (
     RequestLogger,  # Adjust the import based on the actual module structure
 )
-from robot.api import logger
 
 
 class TestRequestLogger(unittest.TestCase):
     def setUp(self):
         self.mock_master = Mock()
-        self.logger = RequestLogger(self.mock_master)
+        self.req_logger = RequestLogger(self.mock_master)
 
     def test_add_to_blocklist(self):
         url = "http://example.com"
-        self.logger.add_to_blocklist(url)
-        self.assertIn(url, self.logger.block_list)
+        self.req_logger.add_to_blocklist(url)
+        self.assertIn(url, self.req_logger.block_list)
 
     def test_remove_from_blocklist(self):
         url = "http://example.com"
-        self.logger.add_to_blocklist(url)
-        self.logger.remove_from_blocklist(url)
-        self.assertNotIn(url, self.logger.block_list)
+        self.req_logger.add_to_blocklist(url)
+        self.req_logger.remove_from_blocklist(url)
+        self.assertNotIn(url, self.req_logger.block_list)
 
     def test_add_custom_response_item(self):
         url = "http://example.com"
         response = {"status": 200, "body": "OK"}
-        self.logger.add_custom_response_item(
+        self.req_logger.add_custom_response_item(
             "alias", url, None, response["body"], response["status"]
         )
-        self.assertEqual(self.logger.custom_response_list[0].url, url)
+        self.assertEqual(self.req_logger.custom_response_list[0].url, url)
 
     def test_remove_custom_response_item(self):
         url = "http://example.com"
-        self.logger.add_custom_response_item("alias", url, None, "OK", 200)
-        self.logger.remove_custom_response_item("alias")
-        self.assertNotIn(url, [item.url for item in self.logger.custom_response_list])
+        self.req_logger.add_custom_response_item("alias", url, None, "OK", 200)
+        self.req_logger.remove_custom_response_item("alias")
+        self.assertNotIn(
+            url,
+            [item.url for item in self.req_logger.custom_response_list],
+        )
 
     def test_add_response_delay_item(self):
         url = "http://example.com"
         delay = "2s"
-        self.logger.add_response_delay_item("alias", url, delay)
-        self.assertEqual(self.logger.response_delays_list[0].url, url)
+        self.req_logger.add_response_delay_item("alias", url, delay)
+        self.assertEqual(self.req_logger.response_delays_list[0].url, url)
 
     def test_clear_all_proxy_items(self):
-        self.logger.add_to_blocklist("http://example.com")
-        self.logger.add_custom_response_item(
+        self.req_logger.add_to_blocklist("http://example.com")
+        self.req_logger.add_custom_response_item(
             "alias", "http://example.com", None, "OK", 200
         )
-        self.logger.add_response_delay_item("alias", "http://example.com", "2s")
-        self.logger.clear_all_proxy_items()
-        self.assertEqual(len(self.logger.block_list), 0)
-        self.assertEqual(len(self.logger.custom_response_list), 0)
-        self.assertEqual(len(self.logger.response_delays_list), 0)
+        self.req_logger.add_response_delay_item("alias", "http://example.com", "2s")
+        self.req_logger.clear_all_proxy_items()
+        self.assertEqual(len(self.req_logger.block_list), 0)
+        self.assertEqual(len(self.req_logger.custom_response_list), 0)
+        self.assertEqual(len(self.req_logger.response_delays_list), 0)
 
     def test_request_blocked(self):
         url = "http://example.com"
-        self.logger.add_to_blocklist(url)
+        self.req_logger.add_to_blocklist(url)
         flow = Mock()
         flow.request.pretty_url = url
         flow.request.pretty_host = url
-        self.logger.request(flow)
-        flow.kill.assert_called_once()
+        with (
+            patch.object(flow, "kill") as mock_kill,
+            patch.object(logger, "info") as mock_info,
+        ):
+            self.req_logger.request(flow)
+            mock_kill.assert_called_once()
+            mock_info.assert_called_once_with(
+                f"Blocked request for {flow.request.pretty_url}",
+                also_console=self.req_logger.log_to_console,
+            )
 
     def test_response_customized(self):
         url = "http://example.com"
-        response = {"status": 200, "body": "OK"}
-        self.logger.add_custom_response_item(
-            "alias", url, None, response["body"], response["status"]
+        response = {
+            "alias": "alias",
+            "url": url,
+            "headers": None,
+            "status_code": 200,
+            "body": "OK",
+        }
+        self.req_logger.add_custom_response_item(
+            alias=response["alias"],
+            url=response["url"],
+            overwrite_body=response["body"],
+            status_code=response["status_code"],
         )
         flow = Mock()
         flow.request.pretty_url = url
-        self.assertEqual(flow.response.status_code, response["status"])
+        self.assertEqual(self.req_logger.custom_response_list[0], response)
 
     def test_log_warning(self):
-        with self.assertLogs("robot.api", level="WARNING") as cm:
-            self.logger.set_console_logging(True)
-            logger.warn("This is a warning")
-            self.assertTrue(
-                any("This is a warning" in message for message in cm.output)
-            )
-
-
-if __name__ == "__main__":
-    unittest.main()
+        self.req_logger.set_console_logging(True)
+        self.assertTrue(self.req_logger.log_to_console)
+        self.req_logger.set_console_logging(False)
+        self.assertFalse(self.req_logger.log_to_console)
